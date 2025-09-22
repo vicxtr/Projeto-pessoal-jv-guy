bfb0f7d6c5063fd2de47b4ab85fca6be8d6bcf5f
João Victor <joao.silva4011@aluno.ce.gov.br>
2025-09-22 20:37:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let moedas
 let dificuldade = parseInt(prompt("escolha um dificulade (1-facil de 0 a 10, 2-medio de 0 a 20, 3-dificil 0-30"))
 
 if(dificuldade === 1){
-    const numeroAdivinha = Math.random * 10
+    const numeroAdivinha = Math.random() * 10
     alert("se você adivinhar vc ganhará 10 moedas")
     let chute = parseInt(prompt("chute um numero:")) === numeroAdivinha ? chute = true : chute = false
     if(chute === true){
@@ -41,7 +41,7 @@ if(dificuldade === 1){
         alert("você errou! que pena, reinicie a pagina")
     }
 }else if(dificuldade === 2){
-    const numeroAdivinha = Math.random * 20
+    const numeroAdivinha = Math.random() * 20
     alert("se você adivinhar vc ganhará 10 moedas")
     let chute = parseInt(prompt("chute um numero:")) === numeroAdivinha ? chute = true : chute = false
     if(chute === true){
@@ -77,7 +77,7 @@ if(dificuldade === 1){
         alert("você errou! que pena, reinicie a pagina")
     }
 }else if(dificuldade === 3){
-        const numeroAdivinha = Math.random * 30
+        const numeroAdivinha = Math.random() * 30
     alert("se você adivinhar vc ganhará 10 moedas")
     let chute = parseInt(prompt("chute um numero:")) === numeroAdivinha ? chute = true : chute = false
     if(chute === true){
